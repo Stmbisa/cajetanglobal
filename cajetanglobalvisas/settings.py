b1f8421e04408fb16663fc1e30ac8f7227d24bbe
Stmbisa <jcmbisa@gmail.com>
2022-09-26 23:03:37 +0300

    setting

--- cajetanglobalvisas/settings.py
+++ cajetanglobalvisas/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-%7+5cv*wa##ap3j6_f&-$f7e^62g(c)7^4t=3wox^blevz3yax'
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
