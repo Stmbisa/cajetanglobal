fb0a53c75b2342438a6bbf0869fbf3949a8d0928
Stmbisa <jcmbisa@gmail.com>
2022-09-27 02:41:46 +0300

    change allowed hosts

--- cajetanglobalvisas/settings.py
+++ cajetanglobalvisas/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost", "www.cajetan.global","cajetan.global","cajetanglobal-hz8wz.ondigitalocean.app").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost", "www.cajetan.global","cajetan.global").split(",")
 
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
