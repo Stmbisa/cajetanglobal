c433fd94f406026abf97e8e48dd4c35eb9516aaf
Stmbisa <jcmbisa@gmail.com>
2022-12-03 20:53:09 +0300

    menulinks error

--- cajetanglobalvisas/settings.py
+++ cajetanglobalvisas/settings.py
@@ -85,7 +85,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'blog.context_processors.add_variable_to_context',
-                'blog.context_processors.menu_links',
             ],
         },
     },
